cc12d02e36e3f93d03ee9528c20e29ebef5b5fab
Nitin <177527946+Nitin225@users.noreply.github.com>
2026-07-01 23:36:56 +0530

    Support single video processing

--- process_videos.py
+++ process_videos.py
@@ -1,16 +1,49 @@
 import os
 import subprocess
 
-def process_videos():
+
+def process_videos(video_path=None):
     os.makedirs("audios", exist_ok=True)
 
     video_extensions = (".mp4", ".mkv", ".avi", ".mov", ".flv", ".webm")
 
-    for file in os.listdir("videos"):
-        video_path = os.path.join("videos", file)
+    # New Upload Mode
+    if video_path is not None:
 
-        # check valid video file
-        if os.path.isfile(video_path) and file.lower().endswith(video_extensions):
+        file = os.path.basename(video_path)
+
+        if not file.lower().endswith(video_extensions):
+            print("Invalid video format.")
+            return None
+
+        file_name = os.path.splitext(file)[0]
+        output_path = os.path.join("audios", f"{file_name}.mp3")
+
+        print(f"Processing: {file}")
+
+        try:
+            subprocess.run(
+                [
+                    "ffmpeg",
+                    "-y",
+                    "-i",
+                    video_path,
+                    output_path,
+                ],
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            print(f"Error processing: {file}")
+            return None
+
+        return output_path
+
+    # Old Mode
+    for file in os.listdir("videos"):
+
+        current_video_path = os.path.join("videos", file)
+
+        if os.path.isfile(current_video_path) and file.lower().endswith(video_extensions):
 
             file_name = os.path.splitext(file)[0]
             output_path = os.path.join("audios", f"{file_name}.mp3")
@@ -18,12 +51,16 @@ def process_videos():
             print(f"Processing: {file}")
 
             try:
-                subprocess.run([
-                    "ffmpeg",
-                    "-y",
-                    "-i", video_path,
-                    output_path
-                ], check=True)
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-y",
+                        "-i",
+                        current_video_path,
+                        output_path,
+                    ],
+                    check=True,
+                )
 
             except subprocess.CalledProcessError:
                 print(f"Error processing: {file}")
