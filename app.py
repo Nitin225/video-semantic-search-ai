b35e09e0fe988c320ea8f547043a596f3364705b
Nitin <177527946+Nitin225@users.noreply.github.com>
2026-07-02 20:46:12 +0530

    Fix app.py

--- app.py
+++ app.py
@@ -18,17 +18,6 @@ st.set_page_config(
 
 #  Main UI
 
-st.title(" Video Semantic Search AI")
-
-uploaded_file = st.file_uploader(
-    "Choose a video",
-    type=["mp4"],
-)
-
-st.write(uploaded_file)
-
-st.stop()
-
 st.caption(
     "Upload videos, build a searchable knowledge base, and ask natural language questions."
 )
